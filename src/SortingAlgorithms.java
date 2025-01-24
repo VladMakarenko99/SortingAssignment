fecb8fed45d7c4f066386417b54b8afd3a22d35b
VladMakarenko99 <goze23gooz@gmail.com>
2025-01-24 16:12:47 +0100

    Initial commit

---
+++ src/SortingAlgorithms.java
@@ -0,0 +1,45 @@
+/*
+ Vladyslav Kovtun, 19899
+*/
+public class SortingAlgorithms {
+
+    public static Integer[] insertionSort(Integer[] array) {
+        SortedPQ<Integer, Integer> pq = new SortedPQ<>();
+
+        return sortWithPQ(array, pq);
+    }
+
+    public static Integer[] selectionSort(Integer[] array) {
+        UnsortedPQ<Integer, Integer> pq = new UnsortedPQ<>();
+
+        return sortWithPQ(array, pq);
+    }
+
+    private static Integer[] sortWithPQ(Integer[] array, AbstractPriorityQueue<Integer, Integer> pq) {
+        for (int element : array) {
+            pq.insert(element, element);
+        }
+
+        Integer[] sorted = new Integer[array.length];
+        int i = 0;
+
+        while (!pq.isEmpty()) {
+            sorted[i] = pq.removeMin().getKey();
+            i++;
+        }
+
+        return sorted;
+    }
+
+    public static Integer[] heapSort(Integer[] array) {
+        HeapPQ<Integer, Integer> heap = new HeapPQ<>();
+        for (int num : array) {
+            heap.insert(num, num);
+        }
+        Integer[] sorted = new Integer[array.length];
+        for (int i = 0; i < array.length; i++) {
+            sorted[i] = heap.removeMin().getKey();
+        }
+        return sorted;
+    }
+}
